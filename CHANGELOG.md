3f00ad7e310c3e5620785d8bd13568377c4b1c67
Jonathan Hartman <j@p4nt5.com>
2015-05-23 21:25:02 -0700

    Initial commit; freshly generated cookbook

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Spotify Cookbook CHANGELOG
+==========================
+
+v0.0.1 (2015-05-23)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'spotify'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures spotify'
+long_description 'Installs/Configures spotify'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
