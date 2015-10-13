cac56e6eaf8eb5d0bbcf8ad4fa50f3868322d130
Jonathan Hartman <j@p4nt5.com>
2015-10-12 22:38:04 -0700

    Update unit tests for Chef 12.5

--- spec/libraries/provider_spotify_app_debian_spec.rb
+++ spec/libraries/provider_spotify_app_debian_spec.rb
@@ -5,8 +5,9 @@ require_relative '../../libraries/provider_spotify_app_debian'
 
 describe Chef::Provider::SpotifyApp::Debian do
   let(:name) { 'default' }
-  let(:new_resource) { Chef::Resource::SpotifyApp.new(name, nil) }
-  let(:provider) { described_class.new(new_resource, nil) }
+  let(:run_context) { ChefSpec::SoloRunner.new.converge.run_context }
+  let(:new_resource) { Chef::Resource::SpotifyApp.new(name, run_context) }
+  let(:provider) { described_class.new(new_resource, run_context) }
 
   describe '.provides?' do
     let(:platform) { nil }
